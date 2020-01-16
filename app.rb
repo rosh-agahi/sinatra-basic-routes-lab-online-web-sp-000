45495380abacbb28d73d1d00d03dddfd04d04af8
Roshanak Agahi <rosh.agahi@gmail.com>
2020-01-16 23:18:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ class App < Sinatra::Base
   get ('/') { "Hello, World!"}
   get ('/name') {"My name is Roshy."}
   get ('/hometown') {"My hometown is Davis."}
-  get ('/favorite-song') {"My favorite song is 'The Gardener.'"}
+  get ('/favorite-song') {"My favorite song is The Gardener."}
 end
